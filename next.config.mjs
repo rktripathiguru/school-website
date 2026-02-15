6b1ea8ec49567d27f55943175a8f0858eb20b96a
Guru <riteshkaimuri@gmail.com>
2026-02-15 16:59:20 +0530

    Clean config for Vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-  images: {
-    domains: ['localhost'],
-  },
 };
 
 export default nextConfig;
